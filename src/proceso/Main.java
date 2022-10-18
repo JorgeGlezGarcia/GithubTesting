dd5536066f181f28a3e9c9737d8183afcd294192
Jorge González <jorge.gonzalez@sabiogroup.com>
2022-10-18 18:49:54 +0200

    [JOG] Subida de secretos.

--- src/proceso/Main.java
+++ src/proceso/Main.java
@@ -1,5 +1,8 @@
 package proceso;
 
+private final static String JORGE_PASSWORD = "sfghoiwtrwrt3.43";
+private final static String GITHUB_TOKEN = "fafagagq34444444444444qrer";
+
 public class Main {
 	protected static void main(String[] argv) {
 		System.out.println("Hola Marte");
