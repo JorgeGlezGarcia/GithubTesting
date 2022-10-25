b0c3e9a065a636fed59089c10e7ed23c17d99db0
Jorge González <jorge.gonzalez@sabiogroup.com>
2022-10-25 19:10:12 +0200

    Corrección código

--- src/proceso/Main.java
+++ src/proceso/Main.java
@@ -1,10 +1,15 @@
 package proceso;
 
-private final static String JORGE_PASSWORD = "sfghoiwtrwrt3.43";
-private final static String TOKEN = "AHEJRUC473NDKSIR93I4";
-
 public class Main {
+	private final static String JORGE_PASSWORD = "sfghoiwtrwrt3.43";
+	private final static String TOKEN = "AHEJRUC473NDKSIR93I4";
+	
 	protected static void main(String[] argv) {
-		System.out.println("Hola Marte");
+		System.out.println("Hola Mundo - Pass: "+ JORGE_PASSWORD +"Token: "+ TOKEN);
 	}
+	
+	private static void mostrarDatosSecretos() {
+		System.out.println("Hola Mundo - Pass: "+ JORGE_PASSWORD +"Token: "+ TOKEN);
+	}
+	
 }
